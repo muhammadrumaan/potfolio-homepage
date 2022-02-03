5f6edf4801b9917894457404a34ae0c59b196550
muhammadrumaan <96781150+muhammadrumaan@users.noreply.github.com>
2022-02-03 23:13:22 +0530

    Add files via upload

---
+++ navbarscript.js
@@ -0,0 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+const navlist = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click',  () => {
+  navlist.classList.toggle('nav-links-active')
+});
+console.log(2+3);
